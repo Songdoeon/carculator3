289670563ca8bc015da36eb5561e2564201c8520
Songdoeon <ehdjs9583@naver.com>
2023-03-10 17:06:13 +0900

    개발환경-git/1

---
+++ Carculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Carculator{
+    public static void main(){
+        int a,b,c;
+        String operator;
+
+        System.out.println("숫자를 입력해주세요");
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("숫자를 입력해주세요");
+        a = sc.nextInt();
+        System.out.println("기호를 입력해주세요");
+        operator = sc.next();
+        System.out.println("숫자를 입력해주세요");
+        b = sc.nextInt();
+        
+        System.out.println("출력 : ");
+    }
+    public int add(int a, int b){
+        //add
+        return 0;
+    }
+    public int sub(int a, int b){
+        //sub
+        return 0;
+    }
+    public int multi(int a, int b){
+        //multi
+        return 0;
+    }
+    public int div(int a, int b){
+        //div
+        return 0;
+    }
+}
